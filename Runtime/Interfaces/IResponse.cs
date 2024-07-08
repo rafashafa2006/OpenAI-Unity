b63d36264e34b1010608db5da92b7acf938add8f
rafashafa2006 <yaraedusainsdev@gmail.com>
2024-07-09 02:28:12 +0700

    Update IResponse.cs
    esan kesalahan ini terjadi karena penggunaan modifier public pada properti dalam antarmuka, yang tidak diperbolehkan. Dalam antarmuka, semua anggota secara implisit bersifat public, jadi tidak perlu dan tidak boleh menyertakan modifier public.
    
    Untuk memperbaiki kesalahan ini, Anda harus menghapus kata kunci public dari properti Warning. Berikut adalah versi yang benar dari antarmuka IResponse:

--- Runtime/Interfaces/IResponse.cs
+++ Runtime/Interfaces/IResponse.cs
@@ -3,6 +3,6 @@ namespace OpenAI
     public interface IResponse
     {
         ApiError Error { get; set; }
-        public string Warning { get; set; }
+        string Warning { get; set; }
     }
 }
